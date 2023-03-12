ed27f9542501c6c6392955b6ab889e1b2c46677c
yoon <ysem1007@naver.com>
2023-03-12 23:01:01 +0900

    0312

--- 10994.py
+++ 10994.py
@@ -1,2 +1,30 @@
 import sys
 n = int(sys.stdin.readline())
+
+def draw(n, idx) :
+    if n == 1:
+        star[idx][idx] = '*'
+        return 
+    l = 4 * n - 3
+    
+    for i in range(idx, l + idx) :
+        star[idx][i] = '*'
+        star[idx+l-1][i] = '*'
+        
+        star[i][idx] = '*'
+        star[i][idx+l-1] = '*'
+    
+    return draw(n-1, idx+2)
+
+len = 4 * n - 3
+
+star = [[' '] * len for _ in range (len)]
+
+draw(n, 0)
+
+for i in range(len) :
+    for j in range(len) :
+        print(star[i][j], end='')
+    print()
+        
+    

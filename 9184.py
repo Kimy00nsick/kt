486b8b038b2355a5dec78e87b7caa5766c83885e
yoon <ysem1007@naver.com>
2023-04-04 23:48:06 +0900

    0404

---
+++ 9184.py
@@ -0,0 +1,10 @@
+# import sys
+
+# def w(a, b, c) :
+    
+# while True :
+#     a , b, c = map(int, sys.stdin.readline().split())
+#     print('w(',a, b, c,') = ', w(a, b, c))
+#     if a == -1 and b == -1 and c == -1 :
+#         break
+
